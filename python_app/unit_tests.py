2a44e0b16346a6e37f7ebdb3567d7ebff59f41d0
vagrant <vagrant@master-node>
2023-05-08 17:53:30 +0000

    adding unit test with artifact report

---
+++ python_app/unit_tests.py
@@ -0,0 +1,15 @@
+import os
+import unittest
+
+class TestCurrentFolder(unittest.TestCase):
+    
+    def test_files_in_current_folder(self):
+        # Get the list of files in the current folder
+        files = os.listdir('.')
+        # Check if the list is not empty
+        self.assertGreater(len(files), 0, "Unit test completed successfully !")
+
+if __name__ == '__main__':
+    print("Running tests...", "\n")
+    
+    unittest.main()
